c1c6528e8295cb2c6f46fa2c36dfb51cd92c0a3f
jenin ahmad <jenin.ahmed.2002@hotmail.com>
2023-02-04 22:34:12 +0200

    .

--- src/ProjectsManagement.Application/ProjectHistory/ProjectHistoryAppService.cs
+++ src/ProjectsManagement.Application/ProjectHistory/ProjectHistoryAppService.cs
@@ -79,32 +79,32 @@ namespace ProjectsManagement.ProjectHistory
             if (input.ProjectSupervisorsId > 0)
             {
                 var Name = _ProjectSupervisorsrepository.GetAll().Include(x=>x.Supervisor).Where(x => x.Id == input.ProjectSupervisorsId).Select(x => x.Supervisor.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project [{ProjectName}]";
+                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  ";
             }
            else if (input.ProjectWorkersId > 0)
             {
                 var Name = _ProjectWorkersrepository.GetAll().Include(x => x.Worker).Where(x => x.Id == input.ProjectWorkersId).Select(x => x.Worker.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project [{ProjectName}]";
+                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  ";
             }
            else if (input.SprintId>0)
             {
                 var Name = _Sprintsrepository.GetAll().Where(x => x.Id == input.SprintId).Select(x => x.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project [{ProjectName}]";
+                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  ";
             }
            else if (input.JobId > 0)
             {
                 var Name = _Jobsrepository.GetAll().Where(x => x.Id == input.JobId).Select(x => x.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project [{ProjectName}]";
+                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  ";
             }
            else if (input.JobTasksId > 0)
             {
-                var Name = _JobTasksrepository.GetAll().Where(x => x.Id == input.JobTasksId).Select(x => x.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project [{ProjectName}]";
+                var JobTask = _JobTasksrepository.GetAll().Include(x=>x.Job).Where(x => x.Id == input.JobTasksId).FirstOrDefault();
+                input.Description = $"The {input.ProjectHistoryColumns} {JobTask.Name} has been {input.ProjectHistoryActions} in The job {JobTask.Job.Name}  ";
             }
            else if(input.SupervisorNotesId>0)
             {
                 var Name = _SupervisorNotesrepository.GetAll().Where(x => x.Id == input.SupervisorNotesId).Include(x=>x.Supervisor).Select(x => x.Supervisor.Name).FirstOrDefault();
-                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions}  in Project {ProjectName}";
+                input.Description = $"The {input.ProjectHistoryColumns} {Name} has been {input.ProjectHistoryActions} ";
             }
             
 
